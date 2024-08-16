4d362dc51166e27e84f881df67c8f895185bfa72
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-08-16 13:05:46 +0500

    Initial commit for the new repository

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const favMovie = "Avatar";
-let guessMovie = prompt("guess my fav movie");
+// const favMovie = "Avatar";
+// let guessMovie = prompt("guess my fav movie");
 
-while((guessMovie != favMovie) && (guessMovie != "quit")){
-    guessMovie = prompt("wrong guess. please try again");
-}
+// while((guessMovie != favMovie) && (guessMovie != "quit")){
+//     guessMovie = prompt("wrong guess. please try again");
+// }
 
-if(guessMovie == favMovie){
-    console.log("Congratulations you guessed it right!");
+// if(guessMovie == favMovie){
+//     console.log("Congratulations you guessed it right!");
+// }else{
+//     console.log("you quit");
+// }
+
+
+
+
+let todo = ["code", "eat", "sleep"];
+console.log(todo);
+console.log()
+let whatTodo = prompt();
+
+if(whatTodo === "list"){
+    console.log(todo);
+}else if(whatTodo==="add"){
+    let addTodo = prompt();
+    todo.push(addTodo);
+}else if(whatTodo === "delete"){
+
+    let deltTodo = prompt();
+    let num = todo.indexOf(deltTodo); 
+    console.log("item deleted from todo: ", todo.splice(num,1));
+    console.log("remaining todo is : ", todo);        
 }else{
     console.log("you quit");
 }
+
+
 
 
 
