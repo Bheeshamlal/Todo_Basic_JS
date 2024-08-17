cd89b9bb038733eb1be227cf4a2da358f76b0930
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-08-17 15:56:03 +0500

    Added second problem: Delete all occurances of element num in a array

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-// const favMovie = "Avatar";
-// let guessMovie = prompt("guess my fav movie");
+//------  code for favorite Movie -------- :
+// // const favMovie = "Avatar";
+// // let guessMovie = prompt("guess my fav movie");
 
-// while((guessMovie != favMovie) && (guessMovie != "quit")){
-//     guessMovie = prompt("wrong guess. please try again");
-// }
+// // while((guessMovie != favMovie) && (guessMovie != "quit")){
+// //     guessMovie = prompt("wrong guess. please try again");
+// // }
 
-// if(guessMovie == favMovie){
-//     console.log("Congratulations you guessed it right!");
+// // if(guessMovie == favMovie){
+// //     console.log("Congratulations you guessed it right!");
+// // }else{
+// //     console.log("you quit");
+// // }
+
+
+// ---------- Code for Todo ----------------
+
+// let todo = ["code", "eat", "sleep"];
+// console.log(todo);
+// console.log()
+// let whatTodo = prompt();
+
+// if(whatTodo === "list"){
+//     console.log(todo);
+// }else if(whatTodo==="add"){
+//     let addTodo = prompt();
+//     todo.push(addTodo);
+// }else if(whatTodo === "delete"){
+
+//     let deltTodo = prompt();
+//     let num = todo.indexOf(deltTodo); 
+//     console.log("item deleted from todo: ", todo.splice(num,1));
+//     console.log("remaining todo is : ", todo);        
 // }else{
 //     console.log("you quit");
 // }
 
 
 
+// --------- Code for deletealloccurrencesofelement ‘num’ inagivenarray --------
 
-let todo = ["code", "eat", "sleep"];
-console.log(todo);
-console.log()
-let whatTodo = prompt();
+let arr = [1,2,3,4,2];
+num =2;
 
-if(whatTodo === "list"){
-    console.log(todo);
-}else if(whatTodo==="add"){
-    let addTodo = prompt();
-    todo.push(addTodo);
-}else if(whatTodo === "delete"){
-
-    let deltTodo = prompt();
-    let num = todo.indexOf(deltTodo); 
-    console.log("item deleted from todo: ", todo.splice(num,1));
-    console.log("remaining todo is : ", todo);        
-}else{
-    console.log("you quit");
+for(i=0;i<=arr.length;i++){
+    if(arr[i]==2){
+        arr.splice(i,1);
+    }
 }
+console.log(arr);
 
-
-
-
-
-
+// ---------- Code for no of digits in a number
+ 
 
 
 
