1f774b68acf92b3eeb7139a0991732ccb531031d
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-08-18 10:31:02 +0500

    Added 2 other problem codes: how many digits in a number and sum of digits in a number

--- app.js
+++ app.js
@@ -39,18 +39,38 @@
 
 // --------- Code for deletealloccurrencesofelement ‘num’ inagivenarray --------
 
-let arr = [1,2,3,4,2];
-num =2;
+// let arr = [1,2,3,4,2];
+// num =2;
 
-for(i=0;i<=arr.length;i++){
-    if(arr[i]==2){
-        arr.splice(i,1);
-    }
+// for(i=0;i<=arr.length;i++){
+//     if(arr[i]==2){
+//         arr.splice(i,1);
+//     }
+// }
+// console.log(arr);
+
+// ---------- Code for no of digits in a number 
+// let num = 245677; // Example number
+// let count = 0;
+
+// while (num > 0) {
+//     count++;
+//     num = Math.floor(num / 10); // Remove the last digit
+// }
+
+// console.log("Number of digits:", count); // Output: 6
+
+
+// ------ code for sum of digits in a number--------
+let num = 234566;
+let sumNum = 0;
+
+while(num >0){
+    sumNum = (num%10)+sumNum;
+    let num = Math.floor(num/10); 
+
 }
-console.log(arr);
-
-// ---------- Code for no of digits in a number
- 
+console.log(sumNum);
 
 
 
